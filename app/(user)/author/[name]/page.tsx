be28b76f4d524ef652587c698ff3efe52e474b52
Noor-26 <nk26982698@gmail.com>
2023-02-10 23:06:06 +0600

    author posts shown on author bio page

--- app/(user)/author/[name]/page.tsx
+++ app/(user)/author/[name]/page.tsx
@@ -2,6 +2,7 @@ import { PortableText } from '@portabletext/react'
 import { groq } from 'next-sanity'
 import Image from 'next/image'
 import React from 'react'
+import AuthorBlogs from '../../../../components/AuthorBlogs'
 import BlogPost from '../../../../components/BlogPost'
 import { RichTextComponents } from '../../../../components/RichTextComponents'
 import imgUrl from '../../../../lib/imgUrl'
@@ -15,26 +16,29 @@ type Props = {
 
 const Author = async ({params:{name}} : Props) => {
     const query = groq `
-    *[_type == "post" && author._ref in *[_type =="author" && name =="Darkfam"]._id ]{...,author->,}
+    *[_type == "post" && author._ref in *[_type =="author" && name =="Darkfam"]._id ]{...,author->,  categories[]->,}
     `
     const details : any = await client.fetch(query,{name})
 
   return (
     <div>
-        <div className="hero max-w-6xl mx-auto px-10 bg-base-200">
+        <div className="hero min-h-[80vh] max-w-6xl mx-auto  bg-base-200">
   <div className="hero-content flex-col lg:flex-row">
-    <div>
+    <div className='w-[35%]'>
 
-  <img src="https://darkfam-portfolio-1.netlify.app/naruto.png" alt="blog image" className='object-contain blob w-[80%] '   />  
+  <img src="https://darkfam-portfolio-1.netlify.app/naruto.png" alt="blog image" className='object-contain blob w-[100%] '   />  
     </div>
-    <div>
+    <div className='w-[70%]'>
       <h1 className="text-5xl heading_text text-[#fb5607] font-bold"> About {details[0].author.name}</h1>
+      <div className='border-l-[#fb5607] border-l-2 pl-[10px] ml-[5px] mt-3'>
+
       <PortableText  value={details[0].author.bio}  components={RichTextComponents} />
+      </div>
     </div> 
   </div>
 </div>
-<div>
-<p>Featured Blogs</p>
+<div className='mt-[10%]'>
+<p className='text-center text-6xl heading_text'>Featured Blogs</p>
 <div  className='w-full md:w-[70%] mx-auto grid gap-3 justify-center lg:grid-cols-2 px-4 py-10'>
 
 {
