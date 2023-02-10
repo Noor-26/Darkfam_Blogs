a6d5d0f265d327293981a9946ed0ced8672473dd
Noor-26 <nk26982698@gmail.com>
2023-02-10 16:12:22 +0600

    improved single post page and responsiveness

--- components/RichTextComponents.tsx
+++ components/RichTextComponents.tsx
@@ -7,9 +7,9 @@ export const RichTextComponents = {
     types: {
         image: ({value}: any) => {
             return (
-                <div className="relative w-[97%]  h-[30rem] m-10 mx-auto">
-                    <Image src={imgUrl(value).url()} alt="blog image" className='object-cover' fill  />  
-                </div>
+                <div className="relative w-[97%] h-[15rem] md:h-[30rem] m-10 mx-auto">
+                    <Image src={imgUrl(value).url()} alt="blog image" className='object-contain md:object-cover' fill  />  
+                </div> 
             )
         }
     },
@@ -22,10 +22,10 @@ export const RichTextComponents = {
         checkmarks: ({children}:any) => <ol className="m-auto text-lg">{children}</ol>,
       },
       block : {
-        h1: ({children}:any) => <h1 className="text-6xl heading_text capitalize py-8 font-bold">{children}</h1>,
-        h2: ({children}:any) => <h2 className="text-5xl capitalize py-8 font-bold heading_text">{children}</h2>,
-        h3: ({children}:any) => <h3 className="text-4xl capitalize py-8 font-bold heading_text">{children}</h3>,
-        h4: ({children}:any) => <h4 className="text-3xl capitalize py-8 font-bold heading_text">{children}</h4>,
+        h1: ({children}:any) => <h1 className="text-4xl md:text-6xl heading_text capitalize py-8 font-bold">{children}</h1>,
+        h2: ({children}:any) => <h2 className="text-3xl md:text-5xl capitalize py-8 font-bold heading_text">{children}</h2>,
+        h3: ({children}:any) => <h3 className="text-2xl md:text-4xl capitalize py-8 font-bold heading_text">{children}</h3>,
+        h4: ({children}:any) => <h4 className="text-xl md:text-3xl capitalize py-8 font-bold heading_text">{children}</h4>,
         
       },
       blockquote: ({children}: any) => <blockquote className="border-l border-primary p-5 my-4">{children}</blockquote>,
